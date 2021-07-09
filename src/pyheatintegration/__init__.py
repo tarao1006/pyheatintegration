73041d9fab36b3e007b1a43582009345032ee7fb
tarao1006 <taiga4562@gmail.com>
2021-07-09 14:53:39 +0900

    Add get_possible_minimum_temp_diff_range to __init__.py

--- src/pyheatintegration/__init__.py
+++ src/pyheatintegration/__init__.py
@@ -3,4 +3,4 @@ from .grand_composite_curve import GrandCompositeCurve  # noqa
 from .line import extract_x, y_range  # noqa
 from .pinch_analyzer import PinchAnalyzer  # noqa
 from .stream import Stream  # noqa
-from .tq_diagram import TQDiagram  # noqa
+from .tq_diagram import TQDiagram, get_possible_minimum_temp_diff_range  # noqa
