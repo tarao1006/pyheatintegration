2357b8f839deaf6e387439c88db2fb94467adbed
tarao1006 <taiga4562@gmail.com>
2021-07-09 11:24:33 +0900

    Update docs version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, tarao1006'
 author = 'tarao1006'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
