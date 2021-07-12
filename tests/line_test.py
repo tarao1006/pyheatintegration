7dbb918235cd39ee30ec44555c9f25b559be600e
tarao1006 <taiga4562@gmail.com>
2021-07-12 17:54:06 +0900

    Add line_test

---
+++ tests/line_test.py
@@ -0,0 +1,32 @@
+import unittest
+
+from src.pyheatintegration.line import y_range, extract_x
+
+
+class TestLine(unittest.TestCase):
+
+    def test_y_range(self):
+        self.assertEqual(
+            y_range([
+                ((0, 0), (1, 1)),
+                ((1, 1), (2, 2)),
+                ((2, 2), (3, 5)),
+                ((3, 3), (5, 8))
+            ]),
+            (0, 8)
+        )
+
+    def test_extract_x(self):
+        self.assertEqual(
+            extract_x([
+                ((0, 0), (1, 1)),
+                ((1, 1), (2, 2)),
+                ((2, 2), (3, 5)),
+                ((3, 3), (5, 8))
+            ]),
+            [0, 1, 2, 3, 5]
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
