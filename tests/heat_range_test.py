21efe609a19efda8b6e07a8ce08749c74a764620
tarao1006 <taiga4562@gmail.com>
2021-07-14 22:03:12 +0900

    Update heat range test

--- tests/heat_range_test.py
+++ tests/heat_range_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.pyheatintegration.heat_range import HeatRange
+from src.pyheatintegration.heat_range import HeatRange, get_merged_heat_ranges
 
 
 class TestHeatRnage(unittest.TestCase):
@@ -10,6 +10,25 @@ class TestHeatRnage(unittest.TestCase):
             HeatRange(0, 10).__repr__(),
             "HeatRange(0, 10)"
         )
+
+
+class TestGetMergedHeatRanges(unittest.TestCase):
+
+    def test_should_success(self):
+        self.assertEqual(
+            get_merged_heat_ranges([
+                [HeatRange(0, 10), HeatRange(10, 30)],
+                [HeatRange(5, 15), HeatRange(15, 40)]
+            ]),
+            [HeatRange(0, 5), HeatRange(5, 10), HeatRange(10, 15), HeatRange(15, 30), HeatRange(30, 40)]
+        )
+
+    def test_should_raise_value_error(self):
+        with self.assertRaises(ValueError):
+            get_merged_heat_ranges([
+                [HeatRange(0, 10), HeatRange(5, 30)],
+                [HeatRange(5, 15), HeatRange(15, 40)]
+            ])
 
 
 if __name__ == '__main__':
