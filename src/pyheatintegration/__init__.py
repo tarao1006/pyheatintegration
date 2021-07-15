c345ca2c35d559cfcf8d413d188fa254d995fe20
tarao1006 <taiga4562@gmail.com>
2021-07-15 22:17:34 +0900

    Add convert_to_excel_data to __init__

--- src/pyheatintegration/__init__.py
+++ src/pyheatintegration/__init__.py
@@ -1,6 +1,6 @@
 from .enums import StreamState, StreamType  # noqa
 from .grand_composite_curve import GrandCompositeCurve  # noqa
-from .line import extract_x, y_range  # noqa
+from .line import convert_to_excel_data, extract_x, y_range  # noqa
 from .pinch_analyzer import PinchAnalyzer  # noqa
 from .stream import Stream  # noqa
 from .tq_diagram import TQDiagram, get_possible_minimum_temp_diff_range  # noqa
