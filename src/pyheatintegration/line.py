349a595a6168579f179d712e1c76d435e80da15c
tarao1006 <taiga4562@gmail.com>
2021-07-15 22:13:27 +0900

    Add convert_to_excel_data

--- src/pyheatintegration/line.py
+++ src/pyheatintegration/line.py
@@ -41,3 +41,35 @@ def extract_x(lines: list[Line]) -> list[float]:
         [0, 1, 2, 3, 5]
     """
     return sorted(list(set(point[0] for point in chain(*lines))))
+
+
+def convert_to_excel_data(lines_: list[Line]) -> tuple[list[float], list[float]]:
+    """直線のリストをx座標のリストとy座標のリストに変換します。
+
+    Args:
+        lines_ (list[Line]): 直線のリスト
+
+    Returns:
+        typle[list[float], list[float]]:
+            x座標のリストとy座標のリスト
+
+    Examples:
+        >>> convert_to_excel_data([((0, 0), (1, 2)), ((1, 2), (3, 3)), ((3, 3), (4, 5))])
+        ([0, 1, 3, 4], [0, 2, 3, 5])
+        >>> convert_to_excel_data([((0, 0), (1, 2)), ((1, 0), (2, 2))])
+        ([0, 1, 1, 2], [0, 2, 0, 2])
+    """
+    lines = sorted(lines_, key=lambda line: line[0][0])
+    x_list = []
+    y_list = []
+
+    for i in range(len(lines)):
+        x_list.append(lines[i][0][0])
+        y_list.append(lines[i][0][1])
+
+        if i != len(lines) - 1 and lines[i][1] == lines[i + 1][0]:
+            continue
+        x_list.append(lines[i][1][0])
+        y_list.append(lines[i][1][1])
+
+    return x_list, y_list
