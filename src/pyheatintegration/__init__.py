e127d202773652e26ca5c21796d31187638ab9d9
tarao1006 <taiga4562@gmail.com>
2021-11-07 20:07:30 +0900

    Add import error

--- src/pyheatintegration/__init__.py
+++ src/pyheatintegration/__init__.py
@@ -1,4 +1,6 @@
 from .enums import StreamState, StreamType  # noqa
+from .errors import (InvalidMinimumApproachTempDiffError, InvalidStreamError,
+                     PyHeatIntegrationError)
 from .grand_composite_curve import GrandCompositeCurve  # noqa
 from .line import convert_to_excel_data, extract_x, y_range  # noqa
 from .pinch_analyzer import PinchAnalyzer  # noqa
